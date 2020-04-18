fd23dc63002565fec5b76ad6c9d0009cf397bb51
James <jwtankersley10@gmail.com>
2020-04-18 09:46:43 -0500

    added to Readme

--- src/core/Home.js
+++ src/core/Home.js
@@ -20,7 +20,7 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 5
   },
   title: {
-    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+    padding:`${theme.spacing.unit * 1}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 1}px`,
     color: theme.palette.text.primary
   },
   media: {
